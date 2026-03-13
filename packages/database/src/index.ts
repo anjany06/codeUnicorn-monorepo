6be00c38ae8930cb6d173844b54505fd65022b4f
Anjany Pandey <anjany.pandey06@gmail.com>
2026-03-13 21:11:01 +0530

    fix prisma import error

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
@@ -20,4 +20,4 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 export { prisma };
-export * from "./generated/prisma/client";
+export * from "./generated/prisma/client.js";
